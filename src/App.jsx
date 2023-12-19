aeb5886316dd643f5c744677ba9f46e6f38b1a78
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2023-12-19 14:58:48 +0100

    add root components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddBook from './AddBook';
+import Book from './Book';
+
+function App() {
+    return (
+        <>
+            <Book />
+            <AddBook />
+        </>
+    );
+}
+
+export default App;
